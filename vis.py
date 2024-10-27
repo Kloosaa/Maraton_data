11eb29a6ec75030b914e3c2a64b537c085275bee
Kloosaa <150732674+Kloosaa@users.noreply.github.com>
2024-10-27 16:29:46 +0100

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 
+# Function to plot histogram
 def plot_histogram(data, column, title, xlabel, ylabel, bins=20):
     plt.figure(figsize=(10, 5))
     sns.histplot(data[column], bins=bins)
@@ -12,6 +14,7 @@ def plot_histogram(data, column, title, xlabel, ylabel, bins=20):
     plt.show()
 
 
+# Function to plot histogram by gender
 def plot_histogram_by_gender(data, column, title, xlabel, ylabel, bins=20):
     plt.figure(figsize=(10, 5))
     sns.histplot(data, x=column, hue="athlete_gender", bins=bins, multiple="stack")
@@ -22,6 +25,7 @@ def plot_histogram_by_gender(data, column, title, xlabel, ylabel, bins=20):
     plt.show()
 
 
+# Function to plot average speed distribution
 def plot_average_speed_distribution(data, distances):
     plt.figure(figsize=(14, 10))
     for i, distance in enumerate(distances, start=1):
@@ -39,6 +43,7 @@ def plot_average_speed_distribution(data, distances):
     plt.show()
 
 
+# Function to plot violin plot
 def plot_violin(data):
     plt.figure(figsize=(10, 5))
     sns.violinplot(
@@ -57,6 +62,7 @@ def plot_violin(data):
     plt.show()
 
 
+# Function to plot linear regression
 def plot_linear_regression(data):
     sns.lmplot(
         data=data,
@@ -73,6 +79,7 @@ def plot_linear_regression(data):
     plt.show()
 
 
+# Function to plot average speed by season
 def plot_average_speed_by_season(data):
     plt.figure(figsize=(12, 6))
     mean_speeds = (
@@ -89,26 +96,12 @@ def plot_average_speed_by_season(data):
     plt.show()
 
 
+# Function to plot runners count by age group
 def plot_runners_count_by_age_group(data):
     plt.figure(figsize=(10, 6))
     sns.barplot(data=data, x="Age Group", y="Number of Runners")
-    plt.title("Number of Runners Below and Above 40 Years Old")
+    plt.title("Number of Runners Below and Above 50 Years Old")
     plt.xlabel("Age Group")
     plt.ylabel("Number of Runners")
-    plt.grid(axis="y", linestyle="--", alpha=0.7)
-    plt.show()
-
-
-def plot_nationality_distribution(data):
-
-    plt.figure(figsize=(12, 6))
-    nationality_counts = (
-        data["athlete_country"].value_counts().head(10)
-    )  # Get top 10 nationalities
-    sns.barplot(x=nationality_counts.index, y=nationality_counts.values)
-    plt.title("Top 10 Nationalities in Races")
-    plt.xlabel("Nationality")
-    plt.ylabel("Number of Athletes")
-    plt.xticks(rotation=45)
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.show()
